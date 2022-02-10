aa80426edad44228bf2cbb916e3bfc1333905696
Ian Clark <turbana@gmail.com>
2022-02-10 15:36:56 -0800

    added material changer / blank computer material

---
+++ Assets/Code/Interaction/Handlers/MaterialChangeHandler.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialChangeHandler : BaseGameObject {
+    public MultiWaySwitch source;
+    public int meshMaterialIndex;
+    public Material[] materials;
+    
+    void Start() {
+        source.AddListener(this);
+    }
+
+    protected override void OnChangeEvent(BaseGameObject obj) {
+        int state = ((MultiWaySwitch)obj).State;
+        if (state < 0 || state >= this.materials.Length) {
+            throw new System.Exception("state out of bounds");
+        }
+        MeshRenderer renderer = GetComponent<MeshRenderer>();
+        Material[] mats = renderer.materials;
+        mats[meshMaterialIndex] = this.materials[state];
+        renderer.materials = mats;
+    }
+}
