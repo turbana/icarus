975e8963957698768253ee3c45b444a60067d732
Ian Clark <turbana@gmail.com>
2023-02-11 15:33:15 -0800

    removed old code

--- Assets/Code/Graph/GraphData.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class GraphData : MonoBehaviour {
-    public virtual void GenerateObjects(GraphEdge edge) {}
-    public virtual void Tick(GraphEdge edge) {}
-}
--- Assets/Code/Interaction/IInteractable.cs
+++
@@ -1,6 +0,0 @@
-public interface IInteractable {
-    public void Interact();
-    public void ScrollUp();
-    public void ScrollDown();
-    public CrosshairType GetCrosshair();
-}
