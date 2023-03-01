46986063787a7c398c9c1caa9dbfefa3951829a1
Ian Clark <turbana@gmail.com>
2023-03-01 14:30:08 -0800

    added keypad prefab

--- Assets/Code/Graphics/TestTextAuthoring.cs
+++ Assets/Code/Graphics/TestTextAuthoring.cs
@@ -14,6 +14,7 @@ namespace Icarus.Graphics {
         Label1,
         Label1R,
         Label1L,
+        KeyPad1,
     };
 
     public class TextStyleConfig {
@@ -59,6 +60,15 @@ namespace Icarus.Graphics {
                 Style = FontStyles.Normal,
                 Color = new Color(0f, 0f, 0f),
                 HAlign = HorizontalAlignmentOptions.Left,
+                VAlign = VerticalAlignmentOptions.Middle,
+            },
+            // KeyPad1
+            new TextStyleConfig {
+                Bounds = new Vector2(0.45f, 0.06f),
+                Size = 0.3f,
+                Style = FontStyles.Normal,
+                Color = new Color(1f, 1f, 1f),
+                HAlign = HorizontalAlignmentOptions.Center,
                 VAlign = VerticalAlignmentOptions.Middle,
             },
         };
