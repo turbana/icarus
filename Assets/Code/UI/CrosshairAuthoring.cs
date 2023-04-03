3f39b22aafd72a06edf6c96c8276d647946bb51f
Ian Clark <turbana@gmail.com>
2023-04-03 11:05:05 -0700

    fixed build error in crosshair

--- Assets/Code/UI/CrosshairAuthoring.cs
+++ Assets/Code/UI/CrosshairAuthoring.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using Unity.Entities;
 
@@ -5,22 +6,26 @@ namespace Icarus.UI {
     public class CrosshairAuthoring : MonoBehaviour {
         public Sprite[] Crosshairs;
         
+#if UNITY_EDITOR
         public class CrosshairAuthoringBaker : Baker<CrosshairAuthoring> {
             public override void Bake(CrosshairAuthoring auth) {
-                var entity = GetEntity(TransformUsageFlags.Dynamic);
-                var go = GameObject
-                    .Find("Screen Canvas").transform
-                    .Find("Crosshair")
-                    .gameObject;
-                DependsOn(go);
-                AddComponent<Crosshair>(entity, new Crosshair {
-                        Value = CrosshairType.Normal,
-                    });
-                AddComponentObject<CrosshairConfig>(entity, new CrosshairConfig {
-                        GO = go,
-                        Crosshairs = auth.Crosshairs,
-                    });
+                try {
+                    var entity = GetEntity(TransformUsageFlags.Dynamic);
+                    var go = GameObject
+                        .Find("Screen Canvas").transform
+                        .Find("Crosshair")
+                        .gameObject;
+                    DependsOn(go);
+                    AddComponent<Crosshair>(entity, new Crosshair {
+                            Value = CrosshairType.Normal,
+                        });
+                    AddComponentObject<CrosshairConfig>(entity, new CrosshairConfig {
+                            GO = go,
+                            Crosshairs = auth.Crosshairs,
+                        });
+                } catch (NullReferenceException) {}
             }
         }
+#endif
     }
 }
